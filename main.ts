463ea58f5b40646c9aa06e130b23307d02bbfe6c
deanw2015 <deanwilliams15@outlook.com>
2017-03-03 17:19:50 +0000

    Arrays and operators

---
+++ main.ts
@@ -0,0 +1,39 @@
+import { Observable } from 'rxjs';
+
+let numbers = [1, 2, 3];
+
+let source = Observable.ate((observer) => {
+
+    let index = 0;
+
+    let produceValue = () => {
+        observer.next(numbers[index++]);
+
+        if (index < numbers.length) {
+            setTimeout(produceValue, 500);
+        }
+        else {
+            setTimeout(() => {
+                observer.complete();
+            }, 500);
+        }
+    };
+    produceValue();
+})
+.map(n => {
+    return n * 10
+})
+.filter(n => {
+    return n !== 20;
+});
+
+source.subscribe(
+    value => {
+        console.log(`value: ${value}`);
+    },
+    e => {
+        console.log(`error: ${e}`);
+    },
+    () => {
+        console.log("complete");
+    });
